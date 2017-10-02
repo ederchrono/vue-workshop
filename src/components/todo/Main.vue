f363858f97a46a3db022ae10a7debe262109f293
Ricardo Hernández <ricardohs35@gmail.com>
2017-10-02 16:38:28 -0500

    :sparkles: Add basic todo-list

--- src/components/todo/Main.vue
+++ src/components/todo/Main.vue
@@ -1,3 +1,87 @@
 <template>
-  <h1>TO DO</h1>
+  <div class="container">
+    <h2>Todo List</h2>
+    <form @submit.prevent="addTodo">
+      <div class="input-group">
+        <input v-model="newTodo" class="form-control">
+        <span class="input-group-btn">
+          <button class="btn btn-secondary">Add todo</button>
+        </span>
+      </div>
+    </form>
+
+    <hr>
+    <h4>Not done todos</h4>
+    <ul class="list-group" v-if="pendingTodos.length">
+      <task
+        v-for="(todo, index) in pendingTodos"
+        :todo="todo"
+        :key="index"
+        @onToggled="toggle(todo)"
+      />
+    </ul>
+    <p v-else>There are no pending todos.</p>
+
+    <hr>
+    <h4>Done todos</h4>
+    <ul class="list-group" v-if="completedTodos.length">
+      <task
+        v-for="(todo, index) in completedTodos"
+        :todo="todo"
+        :key="index"
+        @onToggled="toggle(todo)"
+      />
+    </ul>
+    <p v-else>There are no completed todos.</p>
+  </div>
 </template>
+
+<script>
+  import Task from './Task'
+
+  export default {
+    data () {
+      return {
+        newTodo: '',
+        todos: [
+          { description: 'Drink a beer', done: true },
+          { description: 'Deliver the workshop', done: false },
+          { description: 'Learn Vuex', done: false }
+        ]
+      }
+    },
+
+    computed: {
+      completedTodos () {
+        return this.todos.filter(todo => todo.done)
+      },
+
+      pendingTodos () {
+        return this.todos.filter(todo => !todo.done)
+      }
+    },
+
+    methods: {
+      addTodo () {
+        if (!this.newTodo) {
+          return;
+        }
+
+        this.todos.push({
+          description: this.newTodo,
+          done: false
+        })
+
+        this.newTodo = ''
+      },
+
+      toggle (todo) {
+        todo.done = !todo.done
+      }
+    },
+
+    components: {
+      Task
+    }
+  }
+</script>
---
+++ src/components/todo/Task.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="list-group-item">
+    <span :class="{ 'task-done' : todo.done }">{{ todo.description }}</span>
+    <button
+      class="btn btn-sm btn-primary"
+      @click.prevent="toggle"
+    >toggle
+    </button>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['todo'],
+
+    methods: {
+      toggle () {
+        this.$emit('onToggled')
+      }
+    }
+  }
+</script>
+
+<style>
+  .task-done {
+    text-decoration: line-through;
+  }
+</style>
