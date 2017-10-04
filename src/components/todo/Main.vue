05eccc49682b4e955f4637a4b4b89ff0b6022ce9
Ricardo Hernández <ricardohs35@gmail.com>
2017-10-04 16:51:33 -0500

    :hammer: Refactor functionality and component naming

--- src/components/todo/Main.vue
+++ src/components/todo/Main.vue
@@ -1,70 +1,84 @@
 <template>
   <div class="container">
-    <h2>Todo List</h2>
+    <br>
     <form @submit.prevent="addTodo">
       <div class="input-group">
-        <input v-model="newTodo" class="form-control">
-        <span class="input-group-btn">
-          <button class="btn btn-secondary">Add todo</button>
-        </span>
+        <input v-model="newTodo" class="form-control" placeholder="What are you doing...">
+        <div class="input-group-btn">
+          <button class="btn btn-warning">Go!</button>
+        </div>
       </div>
     </form>
 
-    <hr>
-    <h4>Not done todos</h4>
-    <ul class="list-group" v-if="pendingTodos.length">
-      <task
-        v-for="(todo, index) in pendingTodos"
-        :todo="todo"
-        :key="index"
-        @onToggled="toggle(todo)"
-      />
-    </ul>
-    <p v-else>There are no pending todos.</p>
+    <br>
 
-    <hr>
-    <h4>Done todos</h4>
-    <ul class="list-group" v-if="completedTodos.length">
-      <task
-        v-for="(todo, index) in completedTodos"
-        :todo="todo"
-        :key="index"
-        @onToggled="toggle(todo)"
+    <h4>Pending todos</h4>
+    <ul class="list-group">
+      <todo v-for="(todo, index) in pendingTodos"
+            v-bind="todo"
+            :key="index"
+            @onToggled="togglePending(todo)"
+            @onRemoved="remove(todo)"
       />
     </ul>
-    <p v-else>There are no completed todos.</p>
+
+    <br>
+    <h4>Completed todos</h4>
+    <ul class="list-group">
+      <todo v-for="(todo, index) in completedTodos"
+            v-bind="todo"
+            :key="index"
+            @onToggled="toggleCompleted(todo)"
+            @onRemoved="remove(todo)"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-  import Task from './Task'
+  import Todo from './Todo'
 
   export default {
     data () {
       return {
         newTodo: '',
+
         todos: [
           { description: 'Drink a beer', done: true },
-          { description: 'Deliver the workshop', done: false },
-          { description: 'Learn Vuex', done: false }
+          { description: 'Learn VueJS', done: false },
+          { description: 'Deliver the Workshop', done: false }
         ]
       }
     },
 
     computed: {
-      completedTodos () {
-        return this.todos.filter(todo => todo.done)
-      },
-
       pendingTodos () {
         return this.todos.filter(todo => !todo.done)
+      },
+
+      completedTodos () {
+        return this.todos.filter(todo => todo.done)
       }
     },
 
     methods: {
+      remove (todo) {
+        this.todos.splice(this.todos.indexOf(todo), 1)
+      },
+
+      togglePending (todo) {
+        console.log('togled pending')
+        todo.done = !todo.done
+      },
+
+      toggleCompleted (todo) {
+        console.log('togled completed')
+        todo.done = !todo.done
+      },
+
       addTodo () {
         if (!this.newTodo) {
-          return;
+          return
         }
 
         this.todos.push({
@@ -81,7 +95,7 @@
     },
 
     components: {
-      Task
+      Todo
     }
   }
 </script>
---
+++ src/components/todo/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="list-group-item">
+    <button @click="remove" class="btn btn-sm btn-danger">x</button>
+    {{ description }}
+    <button @click="toggle" class="btn btn-sm btn-primary float-right">toggle!</button>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['description', 'done'],
+
+    methods: {
+      toggle () {
+        this.$emit('onToggled')
+      },
+
+      remove () {
+        this.$emit('onRemoved')
+      }
+    }
+  }
+</script>
